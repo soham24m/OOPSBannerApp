bf36bd4dc406a4bc44638304cb6604f393804e97
sohamsiddharthamishra@gmail.com <sohamsiddharthamishra@SOHAMs-MacBook-Air-202.local>
2026-02-17 11:23:29 +0530

    UC1: Print literal OOPS

---
+++ OOPSBannerApp.java
@@ -0,0 +1,14 @@
+/**
+ * OOPSBannerApp - UC1
+ * Prints the literal text "OOPS" to the console.
+ *
+ * @author YourName
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
